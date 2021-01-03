3ffb3299230dcb964a1e9360c364aff1eb8cd421
Anisha-asweety <70945347+Anisha-asweety@users.noreply.github.com>
2021-01-03 13:22:16 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,129 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
+
+        }
+    
+    play(){
+
+                Player.getPlayerInfo();
+                 image(back_img, 0, 0, 1000, 800);
+                 var X =100;
+                 var Y=200;
+                 var index =0;
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    
+                    form.hide();
+                index = index+1;
+                     x=500-allPlayers[plr].distance;
+                     y=500;
+                     
+                     players[index -1].X = x;
+                     players[index -1].Y = y;
+                       
+                if(index === player.index){
+                         
+                         fill("black");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
+                         
+                     }   
+                 
+                 }
+
+                if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+                    player.distance -= 10
+                    player.update();
+                }
+                if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+                    player.distance += 10
+                    player.update();
+                }
+            
+                 if (frameCount % 20 === 0) {
+                     fruits = createSprite(random(100, 1000), 0, 100, 100);
+                     fruits.velocityY = 6;
+                     var rand = Math.round(random(1,5));
+                     switch(rand){
+                         case 1: fruits.addImage("fruit1",fruit1_img);
+                         break;
+                         case 2: fruits.addImage("fruit1", fruit2_img);
+                         break;
+                         case 3: fruits.addImage("fruit1", fruit3_img);
+                         break;
+                         case 4: fruits.addImage("fruit1", fruit4_img);
+                         break;
+                         case 5: fruits.addImage("fruit1", fruit5_img);
+                         break;
+                     }
+                     fruitGroup.add(fruits);
+                     
+                 }
+                 
+                  if (player.index !== null) {
+                     //fill code here, to destroy the objects.
+                     if(fruitGroup.isTouching(fruit1_img)){
+                        fruit1_img.destroyEach();
+                        fruitGroup.destroyEach();
+                        score=score+1;
+                      }
+                      if(fruitGroup.isTouching(fruit2_img)){
+                        fruit2_img.destroyEach();
+                        fruitGroup.destroyEach();
+                        score=score+2;
+                      }
+                      if(fruitGroup.isTouching(fruit3_img)){
+                        fruit3_img.destroyEach();
+                        fruitGroup.destroyEach();
+                        score=score+3;
+                      }
+                      if(fruitGroup.isTouching(fruit4_img)){
+                        fruit4_img.destroyEach();
+                        fruitGroup.destroyEach();
+                        score=score+4;
+                      }
+                      if(fruitGroup.isTouching(fruit5_img)){
+                        fruit5_img.destroyEach();
+                        fruitGroup.destroyEach();
+                        score=score+5;
+                      }
+                    }  
+
+    }
+
+    end(){
+       console.log("Game Ended");
+    }
+}
